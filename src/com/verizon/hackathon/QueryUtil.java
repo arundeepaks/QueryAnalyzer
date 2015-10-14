dd2cc60ce653e1c04ba24c596c2ffe23262067fe
arundeepaks <arundeepak.x.sivanandam@verizon.com>
2015-10-14 08:23:19 -0700

    Update QueryUtil.java

--- src/com/verizon/hackathon/QueryUtil.java
+++ src/com/verizon/hackathon/QueryUtil.java
@@ -7,31 +7,34 @@ public class QueryUtil {
 	public static int getClosingBraceIndex(String str, char endBrace, int fromIndex){
 		
 		System.out.println("str: "+str+", endBrace: "+endBrace+", fromIndex: "+fromIndex);
+		
+		char startBrace = '(';
+		int endBraceIndex = -1;
+		int startBraceCnt = 0;
+		
 		HashMap<String,String> braceTypes = new HashMap<String,String>();
 		braceTypes.put(")", "(");
 		braceTypes.put("}", "{");
 		braceTypes.put("]", "[");
-		
-		int endBraceIndex = -1;
-		int nextStartBraceIndex = -1;
-		char startBrace;
-		
-		endBraceIndex = str.indexOf(endBrace, fromIndex);
-		System.out.println("endBraceIndex: "+endBraceIndex);
-		
 		if (braceTypes.containsKey(String.valueOf(endBrace))){
-			System.out.println("Checking startBrace ");
 			startBrace = braceTypes.get(String.valueOf(endBrace)).charAt(0);
-			nextStartBraceIndex = str.indexOf(startBrace, fromIndex);
 			System.out.println("startBrace: "+startBrace);
-			System.out.println("nextStartBraceIndex: "+nextStartBraceIndex);
-		}
-				
-		if (endBraceIndex != -1 && nextStartBraceIndex > -1 && endBraceIndex > nextStartBraceIndex){
-			System.out.println("Recursive call");
-			return getClosingBraceIndex(str, endBrace, endBraceIndex+1);
 		}
 		
+		for (int i=fromIndex; i<str.length(); i++){
+			if (str.charAt(i) == endBrace){
+				if (startBraceCnt == 0){
+					System.out.println("Reached end brace at: "+i);
+					endBraceIndex = i;
+					break;					
+				}
+				startBraceCnt--;
+				System.out.println("Reached end brace at: "+i+", startBraceCnt = "+startBraceCnt);
+			}else if (str.charAt(i) == startBrace){
+				startBraceCnt++;
+				System.out.println("Reached start Brace at: "+i);
+			}
+		}
 		return endBraceIndex;
 	}
 }
