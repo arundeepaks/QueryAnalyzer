c6a5b079539da0028e144e5cde0f3b0c1b274db5
arundeepaks <arundeepak.x.sivanandam@verizon.com>
2015-10-14 01:08:39 -0700

    Create SourceTable.java

---
+++ src/com/verizon/hackathon/SourceTable.java
@@ -0,0 +1,27 @@
+package com.verizon.hackathon;
+
+public class SourceTable {
+	private String sourceSchema;
+	private String sourceTable;
+	private String transformation;
+	
+	public String getSourceSchema() {
+		return sourceSchema;
+	}
+	public void setSourceSchema(String sourceSchema) {
+		this.sourceSchema = sourceSchema;
+	}
+	public String getSourceTable() {
+		return sourceTable;
+	}
+	public void setSourceTable(String sourceTable) {
+		this.sourceTable = sourceTable;
+	}
+	public String getTransformation() {
+		return transformation;
+	}
+	public void setTransformation(String transformation) {
+		this.transformation = transformation;
+	}
+	
+}
