6a193eb9b5f43cb7d9228738a90baa923668940f
arundeepaks <arundeepak.x.sivanandam@verizon.com>
2015-10-15 15:27:02 +0530

    Update QueryUtil.java

--- src/com/verizon/hackathon/QueryUtil.java
+++ src/com/verizon/hackathon/QueryUtil.java
@@ -6,7 +6,7 @@ public class QueryUtil {
 
 	public static int getClosingBraceIndex(String str, char endBrace, int fromIndex){
 		
-		System.out.println("str: "+str+", endBrace: "+endBrace+", fromIndex: "+fromIndex);
+		System.out.println("getClosingBraceIndex.str: "+str+", endBrace: "+endBrace+", fromIndex: "+fromIndex);
 		
 		char startBrace = '(';
 		int endBraceIndex = -1;
@@ -36,5 +36,85 @@ public class QueryUtil {
 			}
 		}
 		return endBraceIndex;
+	}
+	
+	public static String getToken(String str, String tokenType){
+		
+		System.out.println("getToken.str: "+str);
+		
+		String token = null;
+		
+		if ("COLUMN".equals(tokenType)){
+			// TO DO: Check for braces in future
+			for (int i=0; i<str.length(); i++){
+				if ( !isCharLabel(str.charAt(i)) ){
+					token = str.substring(0,i);
+					break;
+				}
+			}
+		}else if ("OPERATOR".equals(tokenType)){
+			
+			if (str.startsWith("IS NOT")){
+				token = str.substring(0,6);
+			}else if (str.startsWith("IS")){
+				token = str.substring(0,2);
+			}else if (str.startsWith("IN")){
+				token = str.substring(0,2);
+			}else{
+				for (int i=0; i<str.length(); i++){
+					char ch = str.charAt(i);
+					if ( isCharLabel(ch) || isCharSpace(ch) || isCharBrace(ch) || ((int)ch == 60)){
+						// TO DO: Hardcoding 60 for '<' as the label is starting with <. Need to identify a better logic
+						token = str.substring(0,i);
+						break;
+					}
+				}
+			}
+		}else if ("VALUE".equals(tokenType)){
+			str = str.trim();
+			for (int i=0; i<str.length(); i++){
+				char ch = str.charAt(i);
+				if (isCharBrace(ch)){
+					token = str.substring(0,getClosingBraceIndex(str,')',2) + 1);
+					break;
+				}else{
+					if (str.contains("AND")){
+						token = str.substring(0,str.indexOf("AND"));
+					}else{
+						token = str;
+					}
+				}
+			}
+		}
+		
+		System.out.println("token: "+token);
+		return token;
+	}
+	
+	public static boolean isCharLabel(char ch){
+		boolean flag = false;
+		int ascii = ch;
+		if ( (ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122) || (ascii == 95) || (ascii == 46)){
+			flag = true;
+		}
+		return flag;
+	}
+	
+	public static boolean isCharSpace(char ch){
+		boolean flag = false;
+		int ascii = ch;
+		if ( ascii == 32 ){
+			flag = true;
+		}
+		return flag;
+	}
+	
+	public static boolean isCharBrace(char ch){
+		boolean flag = false;
+		int ascii = ch;
+		if ( ascii == 40 ){
+			flag = true;
+		}
+		return flag;
 	}
 }
